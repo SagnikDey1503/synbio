f5e9309d513f6e81fcd150be32401689e31dc0cf
Sagnik Dey <ajaydey1946@gmail.com>
2026-01-13 23:19:32 +0530

    final fix

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,10 +1,5 @@
 // utils/sendMail.js
-console.log('KEY VALUE:', process.env.SENDGRID_API_KEY);
-console.log('KEY TYPE:', typeof process.env.SENDGRID_API_KEY);
-console.log(
-  'STARTS WITH SG:',
-  process.env.SENDGRID_API_KEY?.startsWith('SG.')
-);
+
 
 const sgMail = require('@sendgrid/mail');
 
